fc88f48387959949a20c8c842500ad9bcdcf531a
BlightG <birhane.gulilat@gmail.com>
2023-04-28 01:00:59 +0300

    pycodestyle mod

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,7 @@
 from typing import Union, Tuple
 import math
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ a function to return a tuple of k and and square of v """
     return (k, math.pow(v, 2))
