980ffd4ab8ddf6b228f6a22f21f13bf1162e69b2
BlightG <birhane.gulilat@gmail.com>
2023-05-02 16:07:34 +0300

    task 1 first |=|)

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" a modlue housing the wait_n async function """
+import asyncio
+import random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """ a function that runs wait_random n times
+        and return list of wait times
+    """
+    
+    time = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
+    return time
