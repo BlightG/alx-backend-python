9ab24c9f0dc40ef0e785847576dd216859d60bca
BlightG <birhane.gulilat@gmail.com>
2023-05-03 17:26:12 +0300

    modified task 0 typing

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ a module housing the function async_generator """
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator() -> int:
+async def async_generator() -> Generator[int, None, None]:
     """ a function that asyncrounously yields a random number
         betweeen 1 and 10 for 10 loops
     """
