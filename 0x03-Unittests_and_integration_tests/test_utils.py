8282fd27ae880cc12613bb19747e0928dd5b1b14
BlightG <birhane.gulilat@gmail.com>
2023-05-22 11:13:33 +0300

    task 3 first |=|)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,10 @@
 from typing import List, Mapping, Sequence, Any
 from parameterized import parameterized
 import unittest
+from unittest.mock import patch, MagicMock
+utils = __import__('utils')
 access_nested_map = __import__('utils').access_nested_map
+get_json = __import__('utils').get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,3 +28,15 @@ class TestAccessNestedMap(unittest.TestCase):
                                          Key: Sequence, error: Any):
         with self.assertRaises(error):
             access_nested_map(nested_map, Key)
+
+class TestGetJson(unittest.TestCase):
+    """ a class for testing the GetJson funciton """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, url: str, 
+                      test_payload: Mapping, requests: Any):
+        requests.return_value.json.return_value = test_payload
+        self.assertEqual(get_json(url), test_payload)
