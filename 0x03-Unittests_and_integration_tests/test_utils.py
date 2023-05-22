916061a4be5aa527ab8df5c8bf7b8ee4c6934f33
BlightG <birhane.gulilat@gmail.com>
2023-05-22 13:09:35 +0300

    lint changes

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,8 +27,10 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map_exception(self, nested_map: Mapping,
                                          Key: Sequence, error: Any):
+        """ testes error raised from access nested map """
         with self.assertRaises(error):
             access_nested_map(nested_map, Key)
+
 
 class TestGetJson(unittest.TestCase):
     """ a class for testing the GetJson funciton """
@@ -37,14 +39,18 @@ class TestGetJson(unittest.TestCase):
         ("http://holberton.io", {"payload": False})
     ])
     @patch('utils.requests.get')
-    def test_get_json(self, url: str, 
+    def test_get_json(self, url: str,
                       test_payload: Mapping, requests: Any):
+        """ tests get_json fucniton from utils """
         requests.return_value.json.return_value = test_payload
         self.assertEqual(get_json(url), test_payload)
 
+
 class TestMemoize(unittest.TestCase):
     """ a class for testing the memoize funciton """
+
     def test_memoize(self):
+
         class TestClass:
 
             def a_method(self):
@@ -59,4 +65,3 @@ class TestMemoize(unittest.TestCase):
             self.assertEqual(testcase.a_method(), 42)
             self.assertEqual(testcase.a_method(), 42)
             mock_memo.assert_called_once()
-            
